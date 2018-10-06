d7f238fc822395cfaf60992d54b6b41799071eb8
TosakaUCW <2736275924@qq.com>
2018-10-06 20:01:40 +0800

    modified [BZOJ 1037]

--- BZOJ/[BZOJ 1037] [ZJOI 2008]生日聚会Party.cpp
+++ BZOJ/[BZOJ 1037] [ZJOI 2008]生日聚会Party.cpp
@@ -1,23 +1,27 @@
 #include <stdio.h>
 #include <algorithm>
-
-#define K 20
-#define N 150
-#define P 12345678
 #define Rep(i,x,y) for(register int i=x;i<=y;i++)
 
+using std::min;
+using std::max;
+
+const int K=20;
+const int N=150;
+const int P=12345678;
 int ans,n,m,x,f[N+5][N+5][K+5][K+5];
 
 int main() {
 	scanf("%d%d%d",&n,&m,&x);
-	
+
 	f[0][0][0][0]=1;
 	Rep(i,0,n)
 	Rep(j,0,m)
-	Rep(k,0,std::min(x,i))
-	Rep(l,0,std::min(x,j)) {
-		if(k<x)f[i+1][j][k+1][std::max(l-1,0)]=(f[i+1][j][k+1][std::max(l-1,0)]+f[i][j][k][l])%P;
-		if(l<x)f[i][j+1][std::max(k-1,0)][l+1]=(f[i][j+1][std::max(k-1,0)][l+1]+f[i][j][k][l])%P;
+	Rep(k,0,min(x,i))
+	Rep(l,0,min(x,j)) {
+		if(k<x)
+			f[i+1][j][k+1][max(l-1,0)] = ( f[i][j][k][l] + f[i+1][j][k+1][max(l-1,0)] )%P;
+		if(l<x)
+			f[i][j+1][max(k-1,0)][l+1] = ( f[i][j][k][l] + f[i][j+1][max(k-1,0)][l+1] )%P;
 	}
 
 	Rep(i,0,x)

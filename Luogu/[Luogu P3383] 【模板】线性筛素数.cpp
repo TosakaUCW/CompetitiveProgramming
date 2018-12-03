02b85518a0d17e1b1f14aa813486b2989ed449c6
TosakaUCW <tosakaucw0@gmail.com>
2018-12-03 21:04:37 +0800

    commit

--- Luogu/[Luogu P3383] 【模板】线性筛素数.cpp
+++ Luogu/[Luogu P3383] 【模板】线性筛素数.cpp
@@ -1,41 +1,34 @@
 #include <math.h>
 #include <stdio.h>
 
-#define N 10000
-#define Scan(a) (a=Read())
-#define Rep(i,x,y) for(int i=x;i<=y;i++)
+#define Rep(i, x, y) for (register int i = x; i <= y; i++)
 
-int n,m;
+int n, m;
 
-inline int Read() {
-	int in;
-	char ch, sign;
-	while(ch=getchar(),ch^'-'and(ch<'0' || ch>'9'));
-	if (ch^'-')sign=1,in=ch-'0';
-	else sign=-1,in=0;
-	while(ch=getchar(),ch>='0' and ch<='9')
-		in=(in<<3)+(in<<1)+ch-'0';
-	return in*sign;
+bool is_prime(int num)
+{
+    if (num == 2 or num == 3)
+        return true;
+    if (num == 1 or (num % 6 != 1 and num % 6 != 5))
+        return false;
+    int k = sqrt(num);
+    for (int i = 5; i <= k; i += 6)
+        if (num % i == 0 or num % (i + 2) == 0)
+            return false;
+    return true;
 }
 
-inline bool IsPrime(int num) {
-	if(num==2 or num==3)return true;
-	if(num%6!=1 and num%6!=5 or num==1)return false;
-	int tmp=sqrt(num);
-	for(int i=5; i<=tmp; i+=6)
-		if(num%i==0 or num%(i+2)==0)
-			return false;
-	return true;
+int main()
+{
+    scanf("%d%d", &n, &m);
+    Rep(i, 1, m)
+    {
+        int num;
+        scanf("%d", &num);
+        if (is_prime(num))
+            printf("Yes\n");
+        else
+            printf("No\n");
+    }
+    return 0;
 }
-
-int main() {
-	Scan(n),Scan(m);
-	Rep(i,1,m){
-		int tmp;
-		Scan(tmp);
-		if(IsPrime(tmp))printf("Yes\n");
-		else printf("No\n");
-	}
-	return 0;
-}
-

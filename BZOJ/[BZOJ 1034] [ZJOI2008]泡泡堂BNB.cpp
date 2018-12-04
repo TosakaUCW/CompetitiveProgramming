48f474b199704d273073bdfd67e56fbd4e87133b
TosakaUCW <tosakaucw0@gmail.com>
2018-12-04 21:25:02 +0800

    Changes to be committed: 	modified:   [BZOJ 1034]

--- BZOJ/[BZOJ 1034] [ZJOI2008]泡泡堂BNB.cpp
+++ BZOJ/[BZOJ 1034] [ZJOI2008]泡泡堂BNB.cpp
@@ -23,10 +23,8 @@ int main() {
 	scanf("%d",&n);
 	Rep(i,1,n)scanf("%d",&a[i]);
 	Rep(i,1,n)scanf("%d",&b[i]);
-	
 	std::sort(a+1,a+1+n);
 	std::sort(b+1,b+1+n);
-	
 	printf("%d %d",func(a,b),2*n-func(b,a));
 	return 0;
 }

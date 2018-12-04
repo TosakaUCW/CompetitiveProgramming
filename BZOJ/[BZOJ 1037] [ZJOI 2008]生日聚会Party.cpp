e0de8eff5ca89f81d605c59bb7640a69b8b7e9e5
TosakaUCW <tosakaucw0@gmail.com>
2018-12-04 21:20:36 +0800

    Changes to be committed: 	modified:   [BZOJ 1037]

--- BZOJ/[BZOJ 1037] [ZJOI 2008]生日聚会Party.cpp
+++ BZOJ/[BZOJ 1037] [ZJOI 2008]生日聚会Party.cpp
@@ -1,33 +1,32 @@
 #include <stdio.h>
 #include <algorithm>
-#define Rep(i,x,y) for(register int i=x;i<=y;i++)
 
-using std::min;
-using std::max;
+const int K = 20;
+const int N = 150;
+const int P = 12345678;
 
-const int K=20;
-const int N=150;
-const int P=12345678;
-int ans,n,m,x,f[N+5][N+5][K+5][K+5];
+#define Rep(i, x, y) for (register int i = x; i <= y; i++)
 
-int main() {
-	scanf("%d%d%d",&n,&m,&x);
+int n, m, x,ans;
+int f[N + 5][N + 5][K + 5][K + 5];
 
-	f[0][0][0][0]=1;
-	Rep(i,0,n)
-	Rep(j,0,m)
-	Rep(k,0,min(x,i))
-	Rep(l,0,min(x,j)) {
-		if(k<x)
-			f[i+1][j][k+1][max(l-1,0)] = ( f[i][j][k][l] + f[i+1][j][k+1][max(l-1,0)] )%P;
-		if(l<x)
-			f[i][j+1][max(k-1,0)][l+1] = ( f[i][j][k][l] + f[i][j+1][max(k-1,0)][l+1] )%P;
-	}
-
-	Rep(i,0,x)
-	Rep(j,0,x)
-	ans=(ans+f[n][m][i][j])%P;
-
-	printf("%d",ans);
-	return 0;
+int main()
+{
+    scanf("%d%d%d", &n, &m, &x);
+    f[0][0][0][0] = 1;
+    Rep(i, 0, n)
+        Rep(j, 0, m)
+            Rep(k, 0, std::min(x, i))
+                Rep(l, 0, std::min(x, j))
+                {
+                    if (k < x)
+                        f[i + 1][j][k + 1][std::max(l - 1, 0)] = (f[i + 1][j][k + 1][std::max(l - 1, 0)] + f[i][j][k][l]) % P;
+                    if (l < x)
+                        f[i][j + 1][std::max(k - 1, 0)][l + 1] = (f[i][j + 1][std::max(k - 1, 0)][l + 1] + f[i][j][k][l]) % P;
+                }
+    Rep(i, 0, x)
+        Rep(j, 0, x)
+            ans = (ans + f[n][m][i][j]) % P;
+    printf("%d", ans);
+    return 0;
 }

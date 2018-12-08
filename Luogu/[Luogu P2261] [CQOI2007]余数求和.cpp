30e12b3deb15828802a7535454ec9775f35a8633
TosakaUCW <tosakaucw0@gmail.com>
2018-12-09 00:18:10 +0800

    Changes to be committed: 	modified:   [Luogu P2261]

--- Luogu/[Luogu P2261] [CQOI2007]余数求和.cpp
+++ Luogu/[Luogu P2261] [CQOI2007]余数求和.cpp
@@ -9,12 +9,12 @@ int main()
     ans = n * k;
     for (long long l = 1, r; l <= n; l = r + 1)
     {
-        long long x = k / l;
-        if (x == 0)
+        long long t = k / l;
+        if (t == 0)
             r = n;
         else
-            r = std::min(k / x, n);
-        ans -= (r - l + 1) * x * (l + r) / 2;
+            r = std::min(k / t, n);
+        ans -= t * (r - l + 1) * (l + r) / 2;
     }
     printf("%lld", ans);
     return 0;

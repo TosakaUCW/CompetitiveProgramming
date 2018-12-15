e8be09232d2e08f2d4b6d0280c7967443071f403
TosakaUCW <tosakaucw0@gmail.com>
2018-12-15 13:45:41 +0800

    Changes to be committed: 	modified:   [BZOJ 1008] 	modified:   [BZOJ 1034] 	modified:   [BZOJ 1037] 	new file:   [BZOJ 3505] 	new file:   [Luogu P3166]

---
+++ Luogu/[Luogu P3166] [CQOI2014]数三角形.cpp
@@ -0,0 +1,30 @@
+// luogu-judger-enable-o2
+#include <stdio.h>
+#include <algorithm>
+
+typedef long long ll;
+#define Rep(i, x, y) for (register ll i = x; i <= y; i++)
+
+ll n, m, ans;
+
+ll C(ll n, ll m)
+{
+    ll res = 1;
+    Rep(i, 1, m)
+        res = res * (n - m + i) / i;
+    return res;
+}
+
+int main()
+{
+    scanf("%lld%lld", &m, &n);
+    n++, m++;
+    ans = C(n * m, 3);
+    ans -= m * C(n, 3);
+    ans -= n * C(m, 3);
+    Rep(i, 1, n - 1)
+        Rep(j, 1, m - 1)
+            ans -= (n - i) * (m - j) * (std::__gcd(i, j) - 1) * 2;
+    printf("%lld", ans);
+    return 0;
+}

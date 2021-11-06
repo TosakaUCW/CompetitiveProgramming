eeb81ef702ba301626e66f236e15ddb18a5c905b
TosakaUCW <tosakaucw0@gmail.com>
2021-11-06 15:39:37 +0800

    2021/11/06

--- [HDU 6071] Lazy Running.cpp
+++ [HDU 6071] Lazy Running.cpp
@@ -1,92 +1,99 @@
-#include <stdio.h>
-#include <algorithm>
-#include <memory.h>
-#include <queue>
-
+#include <bits/stdc++.h>
 #define int long long
+#define fi first
+#define se second
+#define ins insert
+#define pb push_back
+#define flu fflush(stdout)
+#define pii std::pair<int, int>
+using std::map;
+using std::priority_queue;
+using std::queue;
+using std::set;
+using std::stack;
+using std::string;
+using std::swap;
+using std::unordered_map;
+using std::unordered_set;
+using std::vector;
+int read(int x = 0, bool f = 0, char ch = getchar())
+{
+    while (ch < 48 or ch > 57)
+        f = ch == 45, ch = getchar();
+    while (48 <= ch and ch <= 57)
+        x = x * 10 + ch - 48, ch = getchar();
+    return f ? -x : x;
+}
+int pow(int x, int k, int P, int res = 1)
+{
+    for (; k; k >>= 1, x = x * x % P)
+        if (k & 1)
+            res = res * x % P;
+    return res;
+}
 
-const int N = 8 + 5;
-const int K = 6e4 + 5;
+const int D = 1e5 + 5;
+const int P = 998244353;
 
-int k;
-int d[N], dis[N][K];
-int head[N], num_edge;
-
+int k, d[5], w;
+int dis[5][D];
+bool vis[5][D];
+vector<pii> g[5];
 struct Node
 {
-    int next, to, dis;
-} edge[N];
-struct node
-{
-    int cur, r, dis;
-    bool friend operator<(node a, node b) { return a.dis > b.dis; }
+    int cur, dis, r;
+    bool friend operator<(Node a, Node b) { return a.dis > b.dis; }
 };
 
-void add_edge(int u, int v, int dis)
+void dijk()
 {
-    edge[++num_edge] = Node{head[u], v, dis}, head[u] = num_edge;
-}
-
-int read()
-{
-    int x = 0, f = 1;
-    char ch = getchar();
-    while ('0' > ch or ch > '9')
-        f = ch == '-' ? -1 : 1, ch = getchar();
-    while ('0' <= ch and ch <= '9')
-        x = x * 10 + ch - 48, ch = getchar();
-    return x * f;
-}
-
-void dijkstra()
-{
-    int w = 2 * std::min(d[1], d[2]);
+    memset(vis, 0, sizeof vis);
     memset(dis, 0x3f, sizeof dis);
-    std::priority_queue<node> Q;
-    Q.push(node{2, 0, 0});
-    dis[2][0] = 0;
-    while (!Q.empty())
+    priority_queue<Node> Q;
+    Q.push({2, 0, 0}), dis[2][0] = 0;
+    for (int u, r; !Q.empty();)
     {
-        node now = Q.top();
-        Q.pop();
-        int u = now.cur, r = now.r;
-        if (now.dis > dis[u][r] or now.dis >= k + w)
+        u = Q.top().cur, r = Q.top().r, Q.pop();
+        if (vis[u][r])
             continue;
-        for (int i = head[u]; i; i = edge[i].next)
+        vis[u][r] = 1;
+        for (auto V : g[u])
         {
-            int v = edge[i].to, nr = (r + edge[i].dis) % w;
-            if (dis[v][nr] > dis[u][r] + edge[i].dis)
+            int v = V.fi, nr = (r + V.se) % w, dist = V.se;
+            if (dis[v][nr] > dis[u][r] + dist)
             {
-                dis[v][nr] = dis[u][r] + edge[i].dis;
-                Q.push(node{v, nr, dis[v][nr]});
+                dis[v][nr] = dis[u][r] + dist;
+                Q.push({v, dis[v][nr], nr});
             }
         }
     }
-    int kmod = k % w, diff = w;
-    for (int i = 0; i < w; ++i)
-        if (dis[2][i] <= k)
-        {
-            if (i >= kmod)
-                diff = std::min(diff, i - kmod);
-            else
-                diff = std::min(diff, w - (kmod - i));
-        }
+}
+
+void solve()
+{
+    k = read();
+    for (int i = 1; i <= 4; i++)
+        d[i] = read(), g[i].clear();
+    for (int i = 1; i <= 4; i++)
+        g[i].pb({i % 4 + 1, d[i]}), g[i % 4 + 1].pb({i, d[i]});
+    w = 2 * std::min(d[1], d[2]), dijk();
+    int ans = LLONG_MAX;
+    for (int i = 0; i <= w; i++)
+        if (dis[2][i] > k)
+            ans = std::min(ans, dis[2][i]);
         else
-            diff = std::min(diff, dis[2][i] - k);
-    printf("%lld\n", k + diff);
+        {
+            int res = (k - dis[2][i]) / w * w + dis[2][i];
+            if ((k - dis[2][i]) % w)
+                res += w;
+            ans = std::min(ans, res);
+        }
+    printf("%lld\n", ans);
 }
 
 signed main()
 {
-    for (int T = read(); T--; dijkstra())
-    {
-        k = read(), num_edge = 0;
-        memset(edge, 0, sizeof edge);
-        memset(head, 0, sizeof head);
-        for (int i = 1; i <= 4; i++)
-            d[i] = read();
-        for (int i = 1; i <= 4; i++)
-            add_edge(i, i % 4 + 1, d[i]), add_edge(i % 4 + 1, i, d[i]);
-    }
+    for (int T = read(); T--; solve())
+        ;
     return 0;
 }

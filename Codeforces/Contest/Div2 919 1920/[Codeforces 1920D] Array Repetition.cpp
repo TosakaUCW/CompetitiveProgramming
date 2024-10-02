3bba2ebad60b3a1722ec688522371bbd4807047a
TosakaUCW <tosakaucw0@gmail.com>
2024-10-03 02:59:24 +0800

    2024/10/03

--- Codeforces/Contest/Div2 919 1920/[Codeforces 1920D] Array Repetition.cpp
+++ Codeforces/Contest/Div2 919 1920/[Codeforces 1920D] Array Repetition.cpp
@@ -1,62 +1,53 @@
-#include <stdio.h>
-#include <string>
-#include <iostream>
-#include <algorithm>
-#include <string.h>
-#include <vector>
-#include <queue>
-#include <set>
-#include <map>
-#define int long long
-int read(int x = 0, int f = 0, char ch = getchar())
-{
+#include <bits/stdc++.h>
+using i64 = long long;
+#define int i64
+#define pb push_back
+#define ep emplace
+#define eb emplace_back
+using std::max, std::min, std::swap;
+using std::cin, std::cout, std::string, std::vector;
+int read(int x = 0, int f = 0, char ch = getchar()) {
     while (ch < 48 or 57 < ch) f = ch == 45, ch = getchar();
     while(48 <= ch and ch <= 57) x = x * 10 + ch - 48, ch = getchar();
     return f ? -x : x;
 }
-const int N = 1e6 + 5;
-int las[N], dp[N];
 
-void solve()
-{
+void solve() {
     int n = read(), q = read();
-    for (int i = 0; i <= n; i++) las[i] = dp[i] = 0;
-    for (int i = 1; i <= n; i++)
-    {
+
+    vector<int> las(n + 1), dp(n + 1);
+
+    for (int i = 1; i <= n; i++) {
         int opt = read(), x = read();
-        if (opt == 1)
-            las[i] = x, dp[i] = dp[i - 1] + 1;
-        else
-            las[i] = las[i - 1],
-            dp[i] = (x + 1) > 2e18 / dp[i - 1] ? (long long)2e18 : dp[i - 1] * (x + 1);
+
+        if (opt == 1) {
+            las[i] = x;
+            dp[i] = dp[i - 1] + 1;
+        } else {
+            las[i] = las[i - 1];
+            dp[i] = (x + 1) > 2e18 / dp[i - 1] ? (i64)2e18 : dp[i - 1] * (x + 1);
+        }
     }
-    for (int k; q--; )
-    {
+
+    for (int k; q--; ) {
         k = read();
-        while (1)
-        {
-            int pos = std::lower_bound(dp + 1, dp + 1 + n, k) - dp;
-            if (dp[pos] == k)
-            {
-                std::cout << las[pos] << ' ';
+        while (1) {
+            int pos = std::lower_bound(dp.begin(), dp.end(), k) - dp.begin();
+
+            if (dp[pos] == k) {
+                cout << las[pos] << " \n"[q == 0];
                 break;
             }
-            if (k % dp[pos - 1] == 0)
-            {
-                std::cout << las[pos - 1] << ' ';
+            if (k % dp[pos - 1] == 0) {
+                cout << las[pos - 1] << " \n"[q == 0];
                 break;
             }
             k %= dp[pos - 1];
         }
     }
-    puts("");
 }
 
-signed main()
-{
-#ifndef ONLINE_JUDGE
-    freopen("D.in", "r", stdin);
-#endif
+signed main() {
     for (int T = read(); T--; solve());
     return 0;
 }

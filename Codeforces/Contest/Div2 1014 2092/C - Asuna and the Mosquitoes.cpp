0c077d29abe22af43fe5c986b152016d62181c3e
TosakaUCW <tosakaucw0@gmail.com>
2025-04-03 19:13:01 +0800

    feat: Add code for Asuna and the Mosquitoes problem solution

---
+++ Codeforces/Contest/Div2 1014 2092/C - Asuna and the Mosquitoes.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using i64 = long long;
+#define int i64
+#define pb push_back
+#define ep emplace
+#define eb emplace_back
+using namespace std;
+template <class T1, class T2> ostream &operator<<(ostream &os, const std::pair<T1, T2> &a) { return os << "(" << a.first << ", " << a.second << ")"; };
+template <class T> ostream &operator<<(ostream &os, const vector<T> &as) { const int sz = as.size(); os << "["; for (int i = 0; i < sz; ++i) { if (i >= 256) { os << ", ..."; break; } if (i > 0) { os << ", "; } os << as[i]; } return os << "]"; }
+template <class T> void pv(T a, T b) { for (T i = a; i != b; ++i) cerr << *i << " "; cerr << '\n'; }
+using pii = std::pair<int, int>;
+#define fi first
+#define se second
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (auto &x : a) cin >> x;
+
+    int mx = *max_element(a.begin(), a.end());
+    int sum = accumulate(a.begin(), a.end(), 0ll);
+    
+    int cnt = 0;
+    for (auto x : a) cnt += (x % 2 == 1);
+
+    int ans = 0;
+    if (cnt == 0 or cnt == n) {
+        ans = mx;
+    } else {
+        ans = sum - cnt + 1;
+    }
+    cout << ans << '\n';
+}
+
+signed main() {
+    std::ios::sync_with_stdio(0);
+    std::cin.tie(0);
+    int T; cin >> T;
+    for (; T--; solve());
+    // solve();
+    return 0;
+}

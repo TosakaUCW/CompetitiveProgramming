4217032adb4824c05b43030475f445b418675076
TosakaUCW <tosakaucw0@gmail.com>
2025-08-27 01:56:19 +0800

    feat: add solution for D - Sliding Tree in Codeforces Contest 1045

---
+++ Codeforces/Contest/Div2 1045 2134/D - Sliding Tree.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using i64 = long long;
+using u64 = unsigned long long;
+using i128 = __int128;
+#define int i64
+#define pb push_back
+#define ep emplace
+#define eb emplace_back
+using namespace std;
+int read(int x = 0, int f = 0, char ch = getchar()) {
+    while (ch < 48 or 57 < ch) f = ch == 45, ch = getchar();
+    while (48 <= ch and ch <= 57) x = x * 10 + ch - 48, ch = getchar();
+    return f ? -x : x;
+}
+#define debug(x) cout << #x << " = " << x << "\n";
+#define vdebug(a) cout << #a << " = "; for (auto x : a) cout << x << " "; cout << "\n";
+template <class T1, class T2> ostream &operator<<(ostream &os, const std::pair<T1, T2> &a) { return os << "(" << a.first << ", " << a.second << ")"; };
+template <class T> ostream &operator<<(ostream &os, const vector<T> &as) { const int sz = as.size(); os << "["; for (int i = 0; i < sz; i++) { if (i >= 256) { os << ", ..."; break; } if (i > 0) { os << ", "; } os << as[i]; } return os << "]"; }
+template <class T> void pv(T a, T b) { for (T i = a; i != b; i++) cerr << *i << " "; cerr << '\n'; }
+using pii = pair<int, int>;
+const int inf = 1e18;
+
+void solve() {
+    int n = read();
+
+    vector<int> deg(n + 1);
+    vector<vector<int>> g(n + 1);
+    for (int i = 1; i < n; i++) {
+        int u = read(), v = read();
+        g[u].eb(v); deg[u]++;
+        g[v].eb(u); deg[v]++;
+    }
+    
+    vector<int> dis(n + 1), par(n + 1);
+    auto dfs = [&](this auto&& self, int u, int fa) -> void {
+        dis[u] = dis[fa] + 1;
+        par[u] = fa;
+        for (int v : g[u]) if (v != fa) {
+            dis[v] = dis[u] + 1;
+            self(v, u);
+        }
+    };
+
+    dfs(1, 0);
+    int x = ranges::max_element(dis) - dis.begin();
+    dfs(x, 0);
+    int y = ranges::max_element(dis) - dis.begin();
+
+    if (dis[y] == n) { cout << "-1\n"; return; }
+
+    vector<int> ond(n + 1);
+    for (int t = y; t; t = par[t]) ond[t] = 1;
+
+    int a = -1, b = -1, c = -1;
+    for (int u = 1; u <= n; u++) {
+        for (int v : g[u]) {
+            if (ond[u] and !ond[v]) {
+                a = par[u];
+                b = u;
+                c = v;
+                
+                cout << a << ' ' << b << ' ' << c << "\n";
+                return;
+            }
+        }
+    }
+}
+
+signed main() {
+    for (int T = read(); T--; solve());
+    // solve();
+    return 0;
+}

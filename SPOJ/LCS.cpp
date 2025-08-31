59ba708516063b5b756a716d3d0d6a598c995830
TosakaUCW <tosakaucw0@gmail.com>
2025-09-01 03:34:50 +0800

    feat: add solution for LCS problem using Suffix Automaton

---
+++ SPOJ/LCS.cpp
@@ -0,0 +1,109 @@
+#include <bits/stdc++.h>
+using i64 = long long;
+using u64 = unsigned long long;
+using i128 = __int128;
+#define int i64
+#define pb push_back
+#define ep emplace
+#define eb emplace_back
+using namespace std;
+int read(int x = 0, int f = 0, char ch = getchar()) {
+    while (ch < 48 or 57 < ch) f = ch == 45, ch = getchar();
+    while (48 <= ch and ch <= 57) x = x * 10 + ch - 48, ch = getchar();
+    return f ? -x : x;
+}
+#define debug(x) cout << #x << " = " << x << "\n";
+#define vdebug(a) cout << #a << " = "; for (auto x : a) cout << x << " "; cout << "\n";
+template <class T1, class T2> ostream &operator<<(ostream &os, const std::pair<T1, T2> &a) { return os << "(" << a.first << ", " << a.second << ")"; };
+template <class T> ostream &operator<<(ostream &os, const vector<T> &as) { const int sz = as.size(); os << "["; for (int i = 0; i < sz; i++) { if (i >= 256) { os << ", ..."; break; } if (i > 0) { os << ", "; } os << as[i]; } return os << "]"; }
+template <class T> void pv(T a, T b) { for (T i = a; i != b; i++) cerr << *i << " "; cerr << '\n'; }
+using pii = pair<int, int>;
+const int inf = 1e18;
+
+struct SAM {
+    static constexpr int ALPHABET_SIZE = 26;
+    struct Node {
+        int len;
+        int link;
+        std::array<int, ALPHABET_SIZE> next;
+        Node() : len{}, link{}, next{} {}
+    };
+    std::vector<Node> t;
+    SAM() { init(); }
+    void init() {
+        t.assign(2, Node());
+        t[0].next.fill(1);
+        t[0].len = -1;
+    }
+    int newNode() {
+        t.emplace_back();
+        return t.size() - 1;
+    }
+    int extend(int p, int c) {
+        if (t[p].next[c]) {
+            int q = t[p].next[c];
+            if (t[q].len == t[p].len + 1) {
+                return q;
+            }
+            int r = newNode();
+            t[r].len = t[p].len + 1;
+            t[r].link = t[q].link;
+            t[r].next = t[q].next;
+            t[q].link = r;
+            while (t[p].next[c] == q) {
+                t[p].next[c] = r;
+                p = t[p].link;
+            }
+            return r;
+        }
+        int cur = newNode();
+        t[cur].len = t[p].len + 1;
+        while (!t[p].next[c]) {
+            t[p].next[c] = cur;
+            p = t[p].link;
+        }
+        t[cur].link = extend(p, c);
+        return cur;
+    }
+    int next(int p, int x) { return t[p].next[x]; }
+    int link(int p) { return t[p].link; }
+    int len(int p) { return t[p].len; }
+    int size() { return t.size(); }
+};
+
+void solve() {
+    string A, B; cin >> A >> B;
+    SAM sam;
+    for (int p = 1; auto ch : A) {
+        if (ch < 'a' or ch > 'z') continue;
+        p = sam.extend(p, ch - 'a');
+    }
+
+    int ans = 0;
+    for (int v = 1, len = 0; auto ch : B) {
+        int c = ch - 'a';
+        if (sam.next(v, c)) {
+            v = sam.next(v, c);
+            len++;
+        } else {
+            while (v and !sam.next(v, c)) {
+                v = sam.link(v);
+            }
+            if (v == 0) {
+                v = 1, len = 0;
+                continue;
+            }
+
+            len = sam.len(v) + 1;
+            v = sam.next(v, c);
+        }
+        ans = max(ans, len);
+    }
+    cout << ans << '\n';
+}
+
+signed main() {
+    // for (int T = read(); T--; solve());
+    solve();
+    return 0;
+}

6f129d3e9e3778a49340eebecf345af3ae61d7f5
TosakaUCW <tosakaucw0@gmail.com>
2025-09-19 18:21:25 +0800

    feat: add solution for new problem using advanced algorithm

---
+++ Codeforces/Gym/2025 ICPC 网络赛 2/E - Zero.cpp
@@ -0,0 +1,118 @@
+#include <bits/stdc++.h>
+using i64 = long long;
+using u64 = unsigned long long;
+using i128 = __int128;
+#define int i64
+#define pb push_back
+#define ep emplace
+#define eb emplace_back
+using namespace std;
+int read(int x = 0, int f = 0, char ch = getchar()) {
+    while (ch < 48 or 57 < ch) f = ch == 45, ch = getchar();
+    while (48 <= ch and ch <= 57) x = x * 10 + ch - 48, ch = getchar();
+    return f ? -x : x;
+}
+#define debug(x) cout << #x << " = " << x << "\n";
+#define vdebug(a) cout << #a << " = "; for (auto x : a) cout << x << " "; cout << "\n";
+template <class T1, class T2> ostream &operator<<(ostream &os, const pair<T1, T2> &a) { return os << "(" << a.first << ", " << a.second << ")"; };
+template <class T> ostream &operator<<(ostream &os, const vector<T> &as) { const int sz = as.size(); os << "["; for (int i = 0; i < sz; i++) { if (i >= 256) { os << ", ..."; break; } if (i > 0) { os << ", "; } os << as[i]; } return os << "]"; }
+template <class T> void pv(T a, T b) { for (T i = a; i != b; i++) cerr << *i << " "; cerr << '\n'; }
+using pii = pair<int, int>;
+const int inf = 1e18;
+template <class T>
+constexpr T power(T a, i64 b) { T res {1}; for (; b; b /= 2, a = a * a) if (b % 2) res = res * a; return res; }
+constexpr i64 mul(i64 a, i64 b, i64 p) { i64 res = a * b - (i64)(1.L * a * b / p) * p; res %= p; if (res < 0) res += p; return res; }
+template <i64 P>
+struct MInt {
+    i64 x;
+    constexpr MInt() : x {0} {}
+    constexpr MInt(i64 x) : x {norm(x % getMod())} {}
+    static i64 Mod;
+    constexpr static i64 getMod() { return P > 0 ? P : Mod; }
+    constexpr static void setMod(i64 Mod_) { Mod = Mod_; }
+    constexpr i64 norm(i64 x) const { if (x < 0) x += getMod(); if (x >= getMod()) x -= getMod(); return x; }
+    constexpr i64 val() const { return x; }
+    constexpr MInt operator-() const { MInt res; res.x = norm(getMod() - x); return res; }
+    constexpr MInt inv() const { return power(*this, getMod() - 2); }
+    constexpr MInt &operator*=(MInt rhs) & { if (getMod() < (1ULL << 31)) x = x * rhs.x % int(getMod()); else x = mul(x, rhs.x, getMod()); return *this; }
+    constexpr MInt &operator+=(MInt rhs) & { x = norm(x + rhs.x); return *this; }
+    constexpr MInt &operator-=(MInt rhs) & { x = norm(x - rhs.x); return *this; }
+    constexpr MInt &operator/=(MInt rhs) & { return *this *= rhs.inv(); }
+    friend constexpr MInt operator*(MInt lhs, MInt rhs) { MInt res = lhs; res *= rhs; return res; }
+    friend constexpr MInt operator+(MInt lhs, MInt rhs) { MInt res = lhs; res += rhs; return res; }
+    friend constexpr MInt operator-(MInt lhs, MInt rhs) { MInt res = lhs; res -= rhs; return res; }
+    friend constexpr MInt operator/(MInt lhs, MInt rhs) { MInt res = lhs; res /= rhs; return res; }
+    friend constexpr std::istream &operator>>(std::istream &is, MInt &a) { i64 v; is >> v; a = MInt(v); return is; }
+    friend constexpr std::ostream &operator<<(std::ostream &os, const MInt &a) { return os << a.val(); }
+    friend constexpr bool operator==(MInt lhs, MInt rhs) { return lhs.val() == rhs.val(); }
+    friend constexpr bool operator!=(MInt lhs, MInt rhs) { return lhs.val() != rhs.val(); }
+    friend constexpr bool operator<(MInt lhs, MInt rhs) { return lhs.val() < rhs.val(); }
+};
+template <>
+i64 MInt<0>::Mod = 998244353;
+constexpr int P = 998244353;
+using Z = MInt<P>;
+
+using mat = array<array<Z, 3>, 3>;
+mat operator * (mat a, mat b) {
+    mat c;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            for (int k = 0; k < 3; k++) {
+                c[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+    return c;
+};
+
+void solve() {
+    int n = read();
+    int m = read();
+
+    Z C = power(Z(2), m);
+    Z x = C - 1;
+    vector<Z> f(5);
+    f[1] = 1;
+    f[2] = 0;
+    for (int i = 3; i <= 4; i++) {
+        f[i] = C * power(x, i - 2) - f[i - 2] * x;
+    }
+
+    if (n <= 4) {
+        cout << f[n] << '\n';
+        return;
+    }
+
+    mat coef {
+        array<Z, 3> {-x, 0, 1},
+        array<Z, 3> {1, 0, 0},
+        array<Z, 3> {0, 0, x * x}
+    };
+    coef = power(coef, (n - 1) / 2 - 1);
+
+    mat u;
+    if (n & 1) {
+        u = {
+           array<Z, 3> {0, 0, f[3]},
+           array<Z, 3> {0, 0, f[1]},
+           array<Z, 3> {0, 0, C * power(x, 3)} 
+        };
+    } else {
+        u = {
+           array<Z, 3> {0, 0, f[4]},
+           array<Z, 3> {0, 0, f[2]},
+           array<Z, 3> {0, 0, C * power(x, 4)}
+        };
+    }
+
+    u = coef * u;
+
+    cout << u[0][2] << '\n';
+}
+
+signed main() {
+    for (int T = read(); T--; solve());
+    // solve();
+    return 0;
+}

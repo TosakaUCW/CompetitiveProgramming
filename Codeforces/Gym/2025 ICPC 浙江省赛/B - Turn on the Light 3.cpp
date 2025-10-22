1be29b85da2aa99392b394fd31fdb8d61e173310
TosakaUCW <tosakaucw0@gmail.com>
2025-10-22 15:41:44 +0800

    feat: add solutions for B - Turn on the Light 3, D - Too Clever by Half, and L - Nailoongs Always Lie problems

---
+++ Codeforces/Gym/2025 ICPC 浙江省赛/B - Turn on the Light 3.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using i64 = long long;
+using u64 = unsigned long long;
+using i128 = __int128;
+#define int i64
+#define pb push_back
+#define ep emplace
+#define eb emplace_back
+using namespace std;
+int read(int x = 0, int f = 0, char ch = getchar()) {
+    while (ch < 48 or 57 < ch) f = ch == 45, ch = getchar();
+    while (48 <= ch and ch <= 57) x = x * 10 + ch - 48, ch = getchar();
+    return f ? -x : x;
+}
+#define debug(x) cout << #x << " = " << x << "\n";
+#define vdebug(a) cout << #a << " = "; for (auto x : a) cout << x << " "; cout << "\n";
+template <class T1, class T2> ostream &operator<<(ostream &os, const pair<T1, T2> &a) { return os << "(" << a.first << ", " << a.second << ")"; };
+template <class T> ostream &operator<<(ostream &os, const vector<T> &as) { const int sz = as.size(); os << "["; for (int i = 0; i < sz; i++) { if (i >= 256) { os << ", ..."; break; } if (i > 0) { os << ", "; } os << as[i]; } return os << "]"; }
+template <class T> void pv(T a, T b) { for (T i = a; i != b; i++) cerr << *i << " "; cerr << '\n'; }
+using pii = pair<int, int>;
+const int inf = 1e18;
+
+const int N = 5e4 + 5;
+
+void solve() {
+    int n = read(), m = read();
+
+    vector<int> vis(n + 1);
+    for (int i = 0; i < m; i++) {
+        int x = read();
+        if (vis[x]) {
+            cout << "the lights are already on!\n";
+            continue;
+        }
+
+        int ans = 0;
+        for (int t = x; t <= n; t += x) {
+            if (!vis[t]) ans++;
+            vis[t] = 1;
+        }
+        cout << ans << '\n';
+    }
+}
+
+signed main() {
+    for (int T = read(); T--; solve());
+    // solve();
+    return 0;
+}
---
+++ Codeforces/Gym/2025 ICPC 浙江省赛/L - Nailoongs Always Lie.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using i64 = long long;
+using u64 = unsigned long long;
+using i128 = __int128;
+#define int i64
+#define pb push_back
+#define ep emplace
+#define eb emplace_back
+using namespace std;
+int read(int x = 0, int f = 0, char ch = getchar()) {
+    while (ch < 48 or 57 < ch) f = ch == 45, ch = getchar();
+    while (48 <= ch and ch <= 57) x = x * 10 + ch - 48, ch = getchar();
+    return f ? -x : x;
+}
+#define debug(x) cout << #x << " = " << x << "\n";
+#define vdebug(a) cout << #a << " = "; for (auto x : a) cout << x << " "; cout << "\n";
+template <class T1, class T2> ostream &operator<<(ostream &os, const pair<T1, T2> &a) { return os << "(" << a.first << ", " << a.second << ")"; };
+template <class T> ostream &operator<<(ostream &os, const vector<T> &as) { const int sz = as.size(); os << "["; for (int i = 0; i < sz; i++) { if (i >= 256) { os << ", ..."; break; } if (i > 0) { os << ", "; } os << as[i]; } return os << "]"; }
+template <class T> void pv(T a, T b) { for (T i = a; i != b; i++) cerr << *i << " "; cerr << '\n'; }
+using pii = pair<int, int>;
+const int inf = 1e18;
+
+const int N = 5e4 + 5;
+
+void solve() {
+    int n = read();
+    vector<int> a(n + 1), ind(n + 1);
+    for (int i = 1; i <= n; i++) {
+        a[i] = read();
+        ind[a[i]]++;
+    }
+
+    queue<int> q;
+    for (int i = 1; i <= n; i++) {
+        if (!ind[i]) q.ep(i);
+    }
+
+    vector<int> vis(n + 1);
+
+    int ans = 0;
+    while (!q.empty()) {
+        int x = q.front(); q.pop();
+        ans++;
+        vis[x] = 1;
+        int y = a[x];
+
+        if (!vis[y]) {
+            vis[y] = 1;
+            ind[a[y]]--;
+            if (!ind[a[y]]) {
+                q.ep(a[y]);
+            }
+        }
+    }
+
+    for (int i = 1; i <= n; i++) {
+        if (vis[i]) continue;
+        
+        int len = 0;
+        for (int j = i; !vis[j]; j = a[j]) {
+            len++, vis[j] = 1;
+        }
+        ans += len / 2;
+    }
+
+    cout << ans << '\n';
+}
+
+signed main() {
+    // for (int T = read(); T--; solve());
+    solve();
+    return 0;
+}
